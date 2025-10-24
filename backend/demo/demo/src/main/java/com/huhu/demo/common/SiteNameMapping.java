24b057081dadfec086a14e1e8b6e8b85684c63b3
hu <dh97249@gmail.com>
2025-10-24 10:21:34 +0900

    feat: add site mapping

--- backend/demo/demo/src/main/java/com/huhu/demo/common/SiteNameMapping.java
+++ backend/demo/demo/src/main/java/com/huhu/demo/common/SiteNameMapping.java
@@ -3,7 +3,9 @@ package com.huhu.demo.common;
 public enum SiteNameMapping {
     FMKOERA("fmkorea", "에펨코리아"),
     DCINSIDE("dcinside", "디시인사이드"),
-    NAVER("naver", "네이버카페");
+    NAVER("naver", "네이버카페"),
+    THEQOO("theqoo", "더쿠"),
+    RULIWEB("ruliweb", "루리웹");
 
     private final String code;
     private final String name;
